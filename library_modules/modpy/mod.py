5f995731c2b73ea69e0984f35cdc3a3d543ea600
orsoxii <orsooo@mailforkids.net>
2022-01-15 11:17:35 +0100

    hfjgsqjgfhsgfjsfgsjfsjhfgsjf

---
+++ library_modules/modpy/mod.py
@@ -0,0 +1,13 @@
+def avg(a, b):
+    return((a + b) / 2)
+def calc(a, b, c):
+    if b == "+":
+        return(a + c)
+    if b == "-":
+        return(a - c)
+    if b == "/":
+        return(a / c)
+    if b == "mod":
+        return(a % c)
+    if b == "^":
+        return(a ** c)
